f4fd2150fc7d73c526b2777f3fabb0b1c8b2b626
Flávio Kon Rosenfeld <flaviokr@gmail.com>
2022-10-16 18:48:32 -0300

    Add a guard clause for when the fj.sh file does not exist

--- src/fj.sh
+++ src/fj.sh
@@ -3,13 +3,19 @@
 source_file=fj.sh
 source_command="source $source_file"
 
+if test ! -f $source_file
+then
+    echo "$0: Could not find a $source_file file in the current directory! You should probably create one :)"
+    exit 1
+fi
+
 if egrep "^$source_command$" $source_file
 then
     echo "$0: You are sourcing $source_file in $source_file - that would lead to an infinite loop!"
     exit 1
 fi
 
-source fj.sh
+$source_command
 
 task="$1"
 shift 1
