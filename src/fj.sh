14b12333fdaa7712dc43344083005a684223dc17
Guto Motta <guto@mailbox.org>
2022-12-23 15:04:13 -0300

    Add listing functionality to fj.sh

--- src/fj.sh
+++ src/fj.sh
@@ -1,7 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+task="$1"
 
 source_file=fj.sh
 source_command="source $source_file"
+
+available_tasks=()
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+GENERIC_ERROR_CODE=1
+INVALID_OPT_ERROR_CODE=2
+
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
 
 if test ! -f $source_file
 then
@@ -15,9 +30,75 @@ then
     exit 1
 fi
 
-$source_command
+__load_tasks() {
+    $source_command
 
-task="$1"
-shift 1
+    for tsk in $(declare -F | cut -d' ' -f3-)
+    do
+        # Do not include private functions in available_tasks
+        [[ "$tsk" == __* ]] && continue
+        available_tasks+=($tsk)
+    done
+}
 
-$task "$@"
+__read_opts() {
+    # Transform long opts in short opts
+    for arg in "$@"
+    do
+        shift
+        case "$arg" in
+            '--help')
+                set -- "$@" '-h'
+                ;;
+            '--list')
+                set -- "$@" '-l'  
+                ;;
+            --*)
+                echo "Invalid option: $arg"
+                exit $INVALID_OPT_ERROR_CODE
+                ;;
+            *) 
+                set -- "$@" "$arg"
+                ;;
+        esac
+    done
+
+    while getopts ":hl" opt; do
+        case "${opt}" in
+            h)
+                echo "At the moment, fj.sh does not have an usage description."
+
+                exit 0
+                ;;
+            l)
+                echo "Tasks defined in your $source_file file are: "
+                echo
+                for tsk in "${available_tasks[@]}"
+                do
+                    echo $tsk
+                done
+                echo
+
+                exit 0
+                ;;
+            ?)
+                echo "Invalid option: -${OPTARG}."
+
+                exit 2
+                ;;
+        esac
+    done
+}
+
+__run_task() {
+    shift 1
+    $task "$@"
+}
+
+main() (
+    __load_tasks
+    __read_opts "$@"
+    __run_task "$@"
+)
+
+main "$@"
