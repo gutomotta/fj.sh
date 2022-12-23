93cb86d430518b22e6fa3cb1c1d0a615e3b111ad
Guto Motta <guto@mailbox.org>
2022-12-23 15:46:18 -0300

    Add task validation feature

--- src/fj.sh
+++ src/fj.sh
@@ -1,45 +1,51 @@
 #!/usr/bin/env bash
 
-VERSION=0.3.0
+VERSION=0.4.0
 
-task="$1"
-
-source_file=fj.sh
-source_command="source $source_file"
-
-available_tasks=()
+GENERIC_ERROR_CODE=1
+INVALID_OPT_ERROR_CODE=2
+INVALID_TASK_ERROR_CODE=3
 
 set -o errexit
 set -o nounset
 set -o pipefail
 
-GENERIC_ERROR_CODE=1
-INVALID_OPT_ERROR_CODE=2
-
 if [[ "${TRACE-0}" == "1" ]]; then
     set -o xtrace
 fi
 
-if test ! -f $source_file
-then
-    echo "$0: Could not find a $source_file file in the current directory! You should probably create one :)"
-    exit $GENERIC_ERROR_CODE
-fi
+main() (
+    task_called="$1"
+    source_file=fj.sh
+    source_command="source $source_file"
+    available_tasks=()
 
-if egrep "^$source_command$" $source_file
-then
-    echo "$0: You are sourcing $source_file in $source_file - that would lead to an infinite loop!"
-    exit $GENERIC_ERROR_CODE
-fi
+    __validate_fjsh_file "$@"
+    __load_tasks
+    __read_opts "$@"
+    __validate_task_called_is_defined
+    __run_task "$@"
+)
 
 __load_tasks() {
     $source_command
 
+    unset -f main
+
+    task_called_is_defined=0
     for tsk in $(declare -F | cut -d' ' -f3-)
     do
         # Do not include private functions in available_tasks
         [[ "$tsk" == __* ]] && continue
+
+        # Store available tasks in a list
         available_tasks+=($tsk)
+
+        # For validating that the task called is defined
+        if [[ "$tsk" == "$task_called" ]]
+        then
+            task_called_is_defined=1
+        fi
     done
 }
 
@@ -102,13 +108,29 @@ __read_opts() {
 
 __run_task() {
     shift 1
-    $task "$@"
+    $task_called "$@"
 }
 
-main() (
-    __load_tasks
-    __read_opts "$@"
-    __run_task "$@"
-)
+__validate_fjsh_file() {
+    if test ! -f $source_file
+    then
+        echo "$0: Couldn't find a $source_file file in the current directory. Please create one and define your tasks first."
+        exit $GENERIC_ERROR_CODE
+    fi
+
+    if egrep "^$source_command$" $source_file
+    then
+        echo "$0: You are sourcing $source_file in $source_file - that would lead to an infinite loop!"
+        exit $GENERIC_ERROR_CODE
+    fi
+}
+
+__validate_task_called_is_defined() {
+    if [[ $task_called_is_defined -eq 0 ]]
+    then
+        echo "The task $task_called is not defined in your $source_file file."
+        exit $INVALID_TASK_ERROR_CODE
+    fi
+}
 
 main "$@"
